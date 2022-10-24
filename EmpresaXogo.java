e0d1f3c0cfb61e2a13ed978791066f8d8ce311a4
Noemí Varela <nvarela@iessanclemente.net>
2022-10-24 17:36:38 +0200

    Engadido o código para calcular o prezo da entrada en Java

---
+++ EmpresaXogo.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class EmpresaXogo {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        calcularEntrada();
+    }
+    
+    public static void calcularEntrada() {
+        int idade;
+        double prezo;
+        
+        System.out.println("Indica a idade do cliente");
+        idade = input.nextInt();
+        if (idade < 0) {
+            System.out.println("A idade non é válida");
+        } else {
+            if (idade < 4) {
+                prezo = 0;
+            } else {
+                if (idade < 18) {
+                    prezo = 5;
+                } else {
+                    prezo = 10;
+                }
+            }
+            System.out.println("O prezo da entrada para o cliente é " + prezo + " euros");
+        }
+    }
+}
